57b1a8a12168f6eec9e49e1ff881502b2d364ac3
Enric Pedrós <enric.pedros@col.vueling.com>
2020-06-11 18:12:49 +0200

    Add project files.

---
+++ Projecte_RevealButton/RevealButton.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projecte_RevealButton
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RevealButton : ContentView
+    {
+        public RevealButton()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await ExteriorFrame.FadeTo(0);
+            ExteriorFrame.IsVisible = false;
+        }
+
+        private async void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            ExteriorFrame.IsVisible = true;
+            await ExteriorFrame.FadeTo(1);
+        }
+    }
+}
