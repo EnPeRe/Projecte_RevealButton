5ba027942c57f7a47a895b67fd8b3deec923df74
Enric Pedrós <enric.pedros@col.vueling.com>
2020-06-12 11:09:40 +0200

    [RB] Collection BottomItems

--- Projecte_RevealButton/RevealButton.xaml.cs
+++ Projecte_RevealButton/RevealButton.xaml.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +11,31 @@ namespace Projecte_RevealButton
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RevealButton : ContentView
     {
+        public ObservableCollection<BottomItem> BottomViews
+        {
+            get { return (ObservableCollection<BottomItem>)GetValue(BottomViewsProperty); }
+            set { SetValue(BottomViewsProperty, value); }
+        }
+
+        public static readonly BindableProperty BottomViewsProperty =
+            BindableProperty.Create(nameof(BottomViews), typeof(ObservableCollection<BottomItem>), typeof(RevealButton),
+                propertyChanged: OnBottomsViewsPropertyChanged);
+
+        private static void OnBottomsViewsPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (RevealButton)bindable;
+            if (newValue is ObservableCollection<BottomItem> bottomCollection)
+            {
+                control.BottomCollection.ItemsSource = bottomCollection;
+                if(bottomCollection.Any())
+                    control.BottomCollection.ItemsLayout = new GridItemsLayout(bottomCollection.Count(), ItemsLayoutOrientation.Horizontal);
+            }
+        }
+
         public RevealButton()
         {
             InitializeComponent();
+            BottomViews = new ObservableCollection<BottomItem>();
         }
 
         private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
@@ -27,6 +48,26 @@ namespace Projecte_RevealButton
         {
             ExteriorFrame.IsVisible = true;
             await ExteriorFrame.FadeTo(1);
+        }
+    }
+
+    public class BottomItem : INotifyPropertyChanged
+    {
+        private string title { get; set; }
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value; OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
